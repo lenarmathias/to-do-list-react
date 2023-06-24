29695a5bbe0c13a8cd53b9580445d7e0b9e669de
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-06-24 11:49:48 +0200

    Added onClick to HideDoneButton

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,12 +1,12 @@
 import "./style.css";
 
-const Buttons = ({tasks, hiddenDone}) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
     tasks.length > 0 && (
         <div className="buttons">
-            <button className="buttons__button">
-                {hiddenDone
-                    ? "Pokaż ukończone"
-                    : "Ukryj ukończone"}
+            <button
+                onClick={toggleHideDone}
+                className="buttons__button">
+                {hideDone ? "Pokaż" : "Ukryj"} ukończone
             </button>
             <button
                 className="buttons__button"
