6f7cdd360f58862d870fe0f838a93dc86e2074e1
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-07-20 14:46:24 +0200

    Added focus on input after clicking submit button

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,18 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FormField, TaskInput, AddTaskButton } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
+        focusInput();
+    };
+
+    const focusInput = () => {
+        inputRef.current.focus();
     };
 
     return (
         <FormField onSubmit={onFormSubmit}>
             <TaskInput
+                ref={inputRef}
                 value={newTaskContent}
                 onChange={({ target }) => { setNewTaskContent(target.value) }}
                 placeholder="Co jest do zrobienia?"
