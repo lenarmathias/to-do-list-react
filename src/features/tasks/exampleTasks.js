848142e49c5977ee733d236dd7cc91b1e4e0fec2
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-08-23 04:02:16 +0200

    Created example tasks in separate file

---
+++ src/features/tasks/exampleTasks.js
@@ -0,0 +1,12 @@
+export const exampleTasks = [
+    {
+        content: 'Oznaczyć zadanie jako ukończone',
+        done: false,
+        id: 'CmTcTjwGejVyfXSEkaNtQ'
+    },
+    {
+        content: 'Ukończyć wszystkie zadania',
+        done: false,
+        id: 'CNSg4rPKjgjKx7Gn1Wo8T'
+    },
+]
